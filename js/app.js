998bcba65f79d85d85ad971a56c99c2df2f5eac9
Nathan Johnson <bitbucket@njohnson.me>
2014-05-05 03:55:15 -0500

    v0.0.1

---
+++ js/app.js
@@ -0,0 +1,40 @@
+var clientID = 'c6558cafa162f2e';
+var clientSecret = '4c8d92cb2f73698cfffe9a6fbaf840e9a5ad2b57';
+
+var imgurUrl = 'https://api.imgur.com/3/';
+
+var Imgur = {
+	set: function(key) {
+		this.apiKey = key;
+	},
+	call: function(endPoint, callback) {
+		var auth = 'Client-ID ' + this.apiKey;
+
+		$.ajax({
+			url: imgurUrl + endPoint,
+			method: 'GET',
+			headers: {
+				Authorization: auth,
+				Accept: 'application/json'
+			},
+			success: function(result) {
+				return callback(result);
+			}
+		});
+	}
+};
+
+Imgur.set(clientID);
+
+Imgur.call('album/ImR5H/images', function(resp) {
+	var data = resp.data;
+	for (var cur in data) {
+		var imageLink = data[cur].link;
+		console.log(imageLink);
+		$('.slides-container').append('<img src="' + imageLink + '">');
+		$('#slideshow').superslides({
+			play: 30000,
+			pagination: false
+		});
+	}
+});
